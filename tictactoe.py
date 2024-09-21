1c96557f6f8998cb7863f840f9d3d468265065d7
ac <ac@lamasin.space>
2024-09-22 00:59:58 +0200

    added computer player

--- tictactoe.py
+++ tictactoe.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python3
+import random, time
 
 input_row = "Please select a row by typing number 0, 1 or 2"
 input_col = "Please select a column by typing number 0, 1 or 2"
 print_player = "You are player {} and your sign is {}"
 print_win = "Player {} won."
 print_draw = "Draw!"
+print_computer = "The Computer makes a move!"
 
 row_print = "| {} | {} | {} |"
 col_seperator = " ___________"
@@ -42,10 +44,12 @@ class Game:
     entries = []
     active = True
     draw = False
+    computer_opponent = True
     
     # the field is initialized with 3 columns and 3 rows like a regular tic tac toe game. This could be changed in the future for different sizes of playing fields.
-    def __init__(self, rows = 3, cols = 3):
+    def __init__(self, rows = 3, cols = 3, computer_opponent = True):
         self.entries = [[' ' for x in range(rows)] for y in range(cols)]
+        self.computer_opponent = computer_opponent
     
     # The function apply_move takes a move of type Move and applies it to the field, if its valid. Otherwise, an error is raised.
     def apply_move(self, move: Move):
@@ -55,6 +59,13 @@ class Game:
         self.draw = self.check_full()
         if (self.check_win(move) or self.draw):
             self.active = False
+    
+    # checks         
+    def is_field_empty(self, row: int, col: int):
+        if (self.entries[row][col] == ' '):
+            return True
+        else: 
+            return False
     
     # This function checks for all possible win conditions: same player symbol in the row of the move, same player symbol in the column of the move, or same symbol in the diagonals.    
     def check_win(self, move: Move):
@@ -77,6 +88,14 @@ class Game:
                 if entry != 'X' and entry != 'O':
                     return False
         return True
+    
+    def get_sign_entries(self, playersign = 'X'):
+        allsigns = []
+        for row_counter, row in enumerate(self.entries):
+            for col_counter, value in enumerate(row):
+                if (self.entries[row_counter][col_counter] == playersign):
+                    allsigns.append([row_counter, col_counter])
+        return allsigns
         
         
 # This function is used to print the game state       
@@ -108,16 +127,55 @@ def ask_usermove(game: Game):
 
     return game
 
+def get_column(twodarray, col):
+    return [row[col] for row in twodarray]
    
+def computermove (game: Game):
+    win_move = calculate_win(game, signs[int(player1_active)])
+    if (win_move):
+        print("Computer: Uh, I found a winning move!")
+        move = Move(win_move[0], win_move[1], signs[int(player1_active)])
+        game.apply_move(move)
+        return game
+    while True:
+        rand_row = random.randrange(0,3)
+        rand_col = random.randrange(0,3)
+        if (game.is_field_empty(rand_row, rand_col)):
+            move = Move(rand_row, rand_col, signs[int(player1_active)])
+            game.apply_move(move)
+            break
+    return game
     
+def calculate_win (game: Game, playersign = 'X'):
+    allsigns = game.get_sign_entries(playersign)
+    for [row, col] in allsigns:
+        row_sign = [i for i, value in enumerate(game.entries[row]) if value == playersign]
+        row_empty =  [i for i, value in enumerate(game.entries[row]) if value == ' ']
+        if len(row_sign) > 1 and row_empty:
+            return [row, row_empty[0]]
+        col_sign = [i for i, value in enumerate(get_column(game.entries, col)) if value == playersign]
+        col_empty = [i for i, value in enumerate(get_column(game.entries, col)) if value == ' ']
+        if len(col_sign) > 1 and col_empty:
+            return [col_empty, col]
+        # now also diagonal I guess....
+    return None
         
+            
+    
+            
 
 def main():
     global game, game_active, player1_active
     game = Game()
     print_game(game)
     while game.active:
-        game = ask_usermove(game)
+        if (game.computer_opponent and not player1_active): 
+            print(print_computer)
+            time.sleep(0.5)
+            game = computermove(game)
+        else:
+            game = ask_usermove(game)
+        
         print_game(game)
         # this condition checks if the end of the game is reached, either because there is a win or a draw
         if not game.active:
